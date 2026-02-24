ce2226ca170d802662428e6e47d72af965ac0ad8
lukyeesu <lukyee456@hotmail.com>
2026-02-25 01:35:06 +0700

    update something

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command, mode }) => {
-  // เช็คว่ากำลัง Build โดยใช้ GitHub Actions หรือไม่
-  const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+export default defineConfig(({ command }) => {
+  // เมื่อคุณสั่ง npm run build (เพื่อจะเอาไป deploy ขึ้น gh-pages) 
+  // ตัวแปร command จะมีค่าเป็น 'build'
+  
+  // แต่ Vercel ก็ใช้คำสั่ง 'build' เหมือนกัน! 
+  // ดังน้ันเราต้องเช็คตัวแปร VERCEL เพื่อแยกว่า Vercel กำลังทำงานอยู่หรือเปล่า
+  const isVercel = process.env.VERCEL === '1';
 
   return {
     plugins: [
@@ -14,7 +18,9 @@ export default defineConfig(({ command, mode }) => {
         },
       }),
     ],
-    // ถ้าเป็น GitHub Actions ให้ใช้ '/Pre-Order/' ถ้าไม่ใช่ (Vercel หรือ Local) ให้ใช้ '/'
-    base: isGitHubActions ? '/Pre-Order/' : '/',
+    // ถ้าเป็นการ build และ ไม่ได้อยู่บน Vercel (หมายความว่าคุณสั่ง npm run build ในเครื่องตัวเองเพื่อจะทำ npm run deploy)
+    // ให้ใช้ '/Pre-Order/'
+    // นอกนั้น (รันบนเครื่อง npm run dev หรือ รันบน Vercel) ให้ใช้ '/'
+    base: (command === 'build' && !isVercel) ? '/Pre-Order/' : '/',
   }
 })
