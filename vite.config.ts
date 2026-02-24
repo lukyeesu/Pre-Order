1f1a1450621a97e0e772b3a9eacaae946d9019bb
lukyeesu <lukyee456@hotmail.com>
2026-02-25 01:33:46 +0700

    update something

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-  base: '/Pre-Order/', // แก้เป็นชื่อ Repo ของคุณ
+export default defineConfig(({ command, mode }) => {
+  // เช็คว่ากำลัง Build โดยใช้ GitHub Actions หรือไม่
+  const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler']],
+        },
+      }),
+    ],
+    // ถ้าเป็น GitHub Actions ให้ใช้ '/Pre-Order/' ถ้าไม่ใช่ (Vercel หรือ Local) ให้ใช้ '/'
+    base: isGitHubActions ? '/Pre-Order/' : '/',
+  }
 })
